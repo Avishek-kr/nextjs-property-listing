5406d9c842934b49965a1264a99b0c9d4a10ecc6
Avishek-kr <ak251997@gmail.com>
2025-01-15 22:13:14 +0530

    added cloudinary

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
 async function addProperty(formData) {
     await connectDB();
@@ -21,7 +22,6 @@ async function addProperty(formData) {
     const images = formData
         .getAll('images')
         .filter(image => image.name !== '')
-        .map(image => image.name);
 
     // Create the propertyData object with embedded seller_info
     const propertyData = {
@@ -49,8 +49,29 @@ async function addProperty(formData) {
             email: formData.get('seller_info.email'),
             phone: formData.get('seller_info.phone'),
         },
-        images,
     };
+
+    const imageUrls = [];
+
+    for (const imageFile of images) {
+        const imageBuffer = await imageFile.arrayBuffer();
+        const imageArray = Array.from(new Uint8Array(imageBuffer));
+        const imageData = Buffer.from(imageArray); 
+
+        // Convert to base64
+        const imagebase64 = imageData.toString('base64');
+
+        // Make request to cloudinary
+        const result = await cloudinary.uploader.upload(
+            `data:image/png;base64,${imagebase64}`,{
+                folder: 'propertyPulse',
+            }
+        );
+
+        imageUrls.push(result.secure_url);
+    }
+
+    propertyData.images = imageUrls;
 
     const newProperty = new Property(propertyData);
     await newProperty.save();
